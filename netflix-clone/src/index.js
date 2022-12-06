6ff9f9824e8ff5ca57225dd44e2ac8a4d257bb3c
sonup97 <sonupandit97@gmail.com>
2022-12-06 08:51:47 +0530

    First Registered Component

--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import NetflixRegisterComponent from './components/NetflixRegisterComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NetflixRegisterComponent/>
   </React.StrictMode>
 );
 
