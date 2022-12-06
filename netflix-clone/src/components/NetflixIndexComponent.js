742bddcae2a7295ab63664934ada1eb7ae3742c2
sonup97 <sonupandit97@gmail.com>
2022-12-06 22:17:42 +0530

    "All component Added"

---
+++ netflix-clone/src/components/NetflixIndexComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './NetflixIndexComponent.css'
+export default function NetflixIndexComponent() {
+  return (
+    <div className='container-fluid'>
+      Index 1
+    </div>
+  )
+}
--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
-import NetflixRegisterComponent from './components/NetflixRegisterComponent';
+import NetflixIndexComponent from './components/NetflixRegisterComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NetflixRegisterComponent/>
+    <NetflixIndexComponent/>
   </React.StrictMode>
 );
 
